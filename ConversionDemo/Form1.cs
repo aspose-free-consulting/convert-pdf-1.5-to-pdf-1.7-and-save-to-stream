bc5823f730f08190b2c2027575a687a07c3568bb
shahzad-latif <softscientist@gmail.com>
2019-11-13 09:09:11 +0500

    adding project files

---
+++ ConversionDemo/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Windows.Forms;
+using Aspose.Pdf;
+using Aspose.Pdf.Text;
+using System.Collections.Generic;
+
+namespace ConversionDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUpload_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd = new OpenFileDialog();
+            fd.Filter = "PDF Files (*.pdf)|*.pdf|All files (*.*)|*.*";
+            fd.InitialDirectory = @"C:\";
+            fd.Title = "Please select a PDF file to upload.";
+            fd.Multiselect = false;
+            if (fd.ShowDialog() == DialogResult.OK)
+            {
+                txtFilePath.Text = fd.FileName;
+            }
+        }
+
+        private void btnCreate_Click(object sender, EventArgs e)
+        {
+            
+            if (txtFilePath.Text != String.Empty)
+            {
+                Document doc = new Document(txtFilePath.Text);
+                System.IO.MemoryStream ms = new System.IO.MemoryStream();
+                bool flag = doc.Convert("ConversionLog.xml", PdfFormat.v_1_7, ConvertErrorAction.Delete);
+                if (flag)
+                {
+                    doc.Save(ms);
+                    MessageBox.Show("Conversion was successful.", "Success");
+                }
+                else
+                {
+                    MessageBox.Show("Conversion was not successful.", "Error");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please Select a PDF File First.", "File Not Found");
+            }
+        }
+    }
+}
---
+++ ConversionDemo/Form1.designer.cs
@@ -0,0 +1,88 @@
+﻿namespace ConversionDemo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.btnCreate = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.Location = new System.Drawing.Point(14, 42);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(302, 20);
+            this.txtFilePath.TabIndex = 5;
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(323, 41);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(134, 23);
+            this.btnUpload.TabIndex = 6;
+            this.btnUpload.Text = "Upload PDF";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.btnUpload_Click);
+            // 
+            // btnCreate
+            // 
+            this.btnCreate.Location = new System.Drawing.Point(168, 93);
+            this.btnCreate.Name = "btnCreate";
+            this.btnCreate.Size = new System.Drawing.Size(134, 23);
+            this.btnCreate.TabIndex = 7;
+            this.btnCreate.Text = "Convert";
+            this.btnCreate.UseVisualStyleBackColor = true;
+            this.btnCreate.Click += new System.EventHandler(this.btnCreate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(470, 182);
+            this.Controls.Add(this.btnCreate);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.txtFilePath);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Conversion by Aspose.PDF";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Button btnCreate;
+    }
+}
+
